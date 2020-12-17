547ab4362cf199678559e5fa4090f20e1571e098
Lyn Al Masri <lyn.almasri@allaboutapps.at>
2020-12-17 09:47:21 +0100

    spm fix

--- gdpr-ios/gdpr_ios.h
+++ gdpr-ios/gdpr_ios.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for GDPR.
-FOUNDATION_EXPORT double GDPRVersionNumber;
+FOUNDATION_EXPORT double gdpr_iosVersionNumber;
 
 //! Project version string for GDPR.
-FOUNDATION_EXPORT const unsigned char GDPRVersionString[];
+FOUNDATION_EXPORT const unsigned char gdpr_iosVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GDPR/PublicHeader.h>
 
