7cd32a681fe50ba50e44e53fadd3f175a2071972
Matthias Buchetics <matthias.buchetics@allaboutapps.at>
2020-12-17 10:42:00 +0100

    removed Info.plist from spm target

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "GDPR", targets: ["GDPR"])
     ],
     targets: [
-        .target(name: "GDPR", dependencies: [], path: "GDPR/")
+        .target(name: "GDPR", dependencies: [], path: "GDPR/", exclude: "GDPR/Info.plist")
     ],
     swiftLanguageVersions: [.v5]
 )
