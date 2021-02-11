57b51adcaea9c2631ea99a36b996836ae7d950d0
Lyn Al Masri <lyn.almasri@allaboutapps.at>
2021-02-11 11:14:13 +0100

    font fix

--- GDPR/GDPRAppearance.swift
+++ GDPR/GDPRAppearance.swift
@@ -17,7 +17,7 @@ public class GDPRAppearance {
     public static var headlineFont = Font.system(size: 17.0).bold()
     public static var bodyFont = Font.system(size: 17)
     public static var linkFont = Font.system(size: 17)
-    public static var serviceBodyFont = Font.system(size: 12)
+    public static var serviceBodyFont = Font.system(size: 15)
     public static var subheaderFont = Font.system(size: 17)
     public static var controlsFont = Font.system(size: 15)
     
